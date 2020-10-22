62460e53200b8033b85b18cb82663422fda8c4d1
Dina Saunders <dina_saunders@mac.com>
2020-10-22 15:57:15 -0400

    Initial server commit with html routes

---
+++ public/assets/js/server.js
@@ -0,0 +1,25 @@
+// Dependencies
+const express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+const app = express();
+const PORT = 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// Routes to HTML Files
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "..", "..", "index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "..", "..", "notes.html"));
+});
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+console.log(`App is listening on http://localhost:${PORT}`);
+});
